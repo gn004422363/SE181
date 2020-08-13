8b155654b13fabee28b1f7bdc3f595b156dc5793
ailinweng <59580326+ailinweng@users.noreply.github.com>
2020-08-12 22:06:29 -0400

    update file

--- chessboard.py
+++ chessboard.py
@@ -135,14 +135,10 @@ def main_menu():
     start = buttons(180, "Start")
     about = buttons(300, "About")
     credit = buttons(420, "Credit")
-    draw = buttons(540, "Offer draw")
-    resign = buttons(660, "Resign")
 
     start.draw_button(screen)
     about.draw_button(screen)
     credit.draw_button(screen)
-    draw.draw_button(screen)
-    resign.draw_button(screen)
 
 def main():
     while True:
@@ -157,6 +153,7 @@ def main():
 
 if __name__ == "__main__":
     pygame.init()
+    pygame.font.init()
     width = 640
     height = 640
     win_height = height + 40
